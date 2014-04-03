88738bb9eaf30abaeb1c95de890329bb9b4d3b41
Adam Dehnel (home) <arsdehnel@gmail.com>
2014-04-03 05:44:45 -0500

    Added footer and gitignore for .scssc files

--- wp-content/themes/peerministryorg/footer.php
+++ wp-content/themes/peerministryorg/footer.php
@@ -5,13 +5,29 @@
  */
 ?>
 
-  <footer>
-      <p>
-        <?php bloginfo('name'); ?> is proudly powered by
-        <a href="http://wordpress.org/">WordPress</a>, and built using the <a href="http://html5boilerplate.com/">HTML5 Boilerplate</a>.
-        <!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. -->
-      </p>
-  </footer>
+    <footer>
+        <nav class="navbar nav-footer">
+            <div class="navbar-inner">
+                <ul class="nav">
+                    <li>National Director - Lyle Griner</li>
+                    <li>( 612 ) 418 - 5572</li>
+                    <li><script type="text/javascript">
+                    //<![CDATA[
+                    var l=new Array();
+                    l[0]='>';l[1]='a';l[2]='/';l[3]='<';l[4]='|103';l[5]='|114';l[6]='|111';l[7]='|46';l[8]='|99';l[9]='|108';l[10]='|108';l[11]='|121';l[12]='|97';l[13]='|100';l[14]='|121';l[15]='|114';l[16]='|101';l[17]='|118';l[18]='|101';l[19]='|64';l[20]='|108';l[21]='|109';l[22]='|112';l[23]='>';l[24]='"';l[25]='|103';l[26]='|114';l[27]='|111';l[28]='|46';l[29]='|99';l[30]='|108';l[31]='|108';l[32]='|121';l[33]='|97';l[34]='|100';l[35]='|121';l[36]='|114';l[37]='|101';l[38]='|118';l[39]='|101';l[40]='|64';l[41]='|108';l[42]='|109';l[43]='|112';l[44]=':';l[45]='o';l[46]='t';l[47]='l';l[48]='i';l[49]='a';l[50]='m';l[51]='"';l[52]='=';l[53]='f';l[54]='e';l[55]='r';l[56]='h';l[57]=' ';l[58]='a';l[59]='<';
+                    for (var i = l.length-1; i >= 0; i=i-1){
+                    if (l[i].substring(0, 1) == '|') document.write("&#"+unescape(l[i].substring(1))+";");
+                    else document.write(unescape(l[i]));}
+                    //]]>
+                    </script></li>
+                    <li><a href="/everyday/content/newsletter" data-toggle="modal-external">E-News</a></li>
+                    <li><a href="http://peerministryleadership.blogspot.com/" target="_blank">Blog</a></li>
+                    <li><a href="http://www.facebook.com/pages/Bloomington-MN/Peer-Ministry/30163575347" target="_blank">Facebook</a></li>
+                    <li><a href="/wp-admin/" data-toggle="modal-external">Log In</a></li>
+                </ul>
+            </div>
+        </nav>
+    </footer>
   <div class="modal-window">
   	<div class="modal-close"><i class="icon-modal-close font-fontawesome"></i></div>
   	<div class="modal-content"></div>
