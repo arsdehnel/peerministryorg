0953985e022b207477c770b2f38ad4b6b82a4981
Adam Dehnel (home) <arsdehnel@gmail.com>
2014-07-10 12:01:30 -0500

    Removed simple modal

--- wp-content/plugins/simple-modal/views/public-page-html.php
+++
@@ -1,6 +0,0 @@
-<div id="simplemodal-window">
-	<div class="modal-header"></div>
-	<div class="modal-body"></div>
-	<div class="modal-footer"><a class="simplemodal-close">Close</a></div>
-</div>
-<div id="simplemodal-overlay"></div>
