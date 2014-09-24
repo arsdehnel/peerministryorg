2fa21d62a4f893379045a50df34cbb4f9ac91b46
Adam Dehnel (home) <arsdehnel@gmail.com>
2014-09-23 22:04:18 -0500

    Removed chromeframe

--- wp-content/themes/peerministryorg/header.php
+++ wp-content/themes/peerministryorg/header.php
@@ -35,10 +35,6 @@
 
 </head>
 <body <?php body_class(); ?>>
-  <!--[if lt IE 7]>
-    <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
-  <![endif]-->
-
   <div id="container">
     <header role="banner" class="site-header">
       <h1><a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a></h1>
